57d40bcf461f6dae9fdc182971f825f4513c28e0
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-08-15 20:15:33 -0400

    refactor: change how entry data is stored in db

--- commands/Senbetsu/upload.js
+++ commands/Senbetsu/upload.js
@@ -28,7 +28,7 @@ class UploadCommand extends Firework.Command {
 		const id = this.bot.genID();
 		let tags = opts.tags || []
 		let source = opts.source
-		let channels = [];
+		let entries = {};
 		let nhDoujin;
 
 		if(!isNaN(digits) && opts.source == 'nh') {
@@ -38,16 +38,15 @@ class UploadCommand extends Firework.Command {
 		}
 
 		const sourcechan = msg.channel.guild.channels.find(c => c.name === source)
-		if(sourcechan) channels.push(sourcechan.id)
+		if(sourcechan) entries[source] = [sourcechan.id]
 
 		for(let i = tags.length - 1; i >= 0; i--) {
 			const tagchan = msg.channel.guild.channels.find(c => c.name === tags[i])
 			if(!tagchan) continue;
-			channels.push(tagchan.id)
+			entries[tags[i]] = [tagchan.id]
 		}
 		// i need to store channel name, channel id and msg id
-		// like: {channels: {'tag': [chanid, msgid]}}
-		await this.bot.db.put(id, JSON.stringify({channels}))
+		// like: {'tag': [chanid, msgid]}
 		if(msg.attachments[0]) {
 			const r = await fetch(msg.attachments[0].url)
 			//for (let i = channels.length - 1; i >= 0; i--) {
@@ -55,15 +54,20 @@ class UploadCommand extends Firework.Command {
 			//}
 		} else {
 			const thumb = await nhDoujin.thumbnail.fetch()
-			for (let i = channels.length - 1; i >= 0; i--) {
-				this.bot.getChannel(channels[i]).createMessage(`
+			const entryids = Object.values(entries)
+			for (let i = entryids.length - 1; i >= 0; i--) {
+				const tagchan = this.bot.getChannel(entryids[i][0])
+				const m = await tagchan.createMessage(`
 UID (for Senbetsu post idenification): **${id}**
 Source: ${source || 'None provided'}
 ${nhDoujin.titles.pretty}
 <${this.sourceSite(source, digits)}>
-Tags/categories: ${channels.map(id => `<#${id}>`).join(' ')}`, {file: thumb, name: `thumb.${nhDoujin.thumbnail.extension}`})
+Tags/categories: ${entryids.map(idsarr => `<#${idsarr[0]}>`).join(' ')}`, {file: thumb, name: `thumb.${nhDoujin.thumbnail.extension}`})
+				entries[tagchan.name][1] = m.id
 			}
 		}
+		this.bot.logger.debug('Adding entry', id, 'to database. Entry channel data:\b\n', JSON.stringify(entries))
+		await this.bot.db.put(id, JSON.stringify(entries))
 		m.edit({content: `✅ Posted to all channels! UID is **${id}**`})
 	}
 
