1a98d56144d6147a026da5a1aa3fa8de61b98329
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-08-16 15:12:15 -0400

    feat: make delete command work with multiple uids

--- commands/Senbetsu/delete.js
+++ commands/Senbetsu/delete.js
@@ -8,11 +8,16 @@ class DeleteCommand extends Firework.Command {
 	}
 
 	async run({msg, args}) {
-		const dbentry = await this.bot.db.get(args[0])
+		const m = await msg.channel.createMessage('Processing deletion from all category channels...')
+		for (let i = args.length - 1; i >= 0; i--) {
+			this.deleteEntry(args[i])
+			m.edit({content: `${args.length === 1 ? 'Entry' : 'Entries'} ${args.join(', ')} ${args.length === 1 ? 'has' : 'have'} been deleted successfully.`})
+		}
+	}
+	async deleteEntry(id) {
+		const dbentry = await this.bot.db.get(id)
 		const entry = JSON.parse(dbentry)
 		const entryids = Object.values(entry.postids)
-		const id = args[0]
-		const m = await msg.channel.createMessage('Processing deletion from all category channels...')
 		for (let i = entryids.length - 1; i >= 0; i--) {
 			const entrymsg = await this.bot.getMessage(entryids[i][0], entryids[i][1])
 			entrymsg.delete()
@@ -20,7 +25,6 @@ class DeleteCommand extends Firework.Command {
 
 		await this.bot.db.del(id)
 		this.bot.logger.debug('Removed entry', id, 'from database.')
-		m.edit({content: `Entry ${id} has been deleted successfully.`})
 	}
 }
 
